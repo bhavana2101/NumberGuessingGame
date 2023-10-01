ce686e6c2ef9a3acb4cadfbf684df0e70a9acbd8
bhavana2101 <140957992+bhavana2101@users.noreply.github.com>
2023-10-01 09:35:41 +0530

    Add files via upload

---
+++ NumberGuessing.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class NumberGuessing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int actualNumber = numberGenerator();
+            System.out.println("Enter any number between 1 and 100 or enter 0 to exit : ");
+            int enteredNumber = sc.nextInt();
+            if(enteredNumber==0){
+                System.out.println("Exiting....");
+                break;
+            }
+            while (enteredNumber != actualNumber && enteredNumber!=0) {
+                if (actualNumber % 2 == 0 && actualNumber < enteredNumber) {
+                    System.out.println("Number is even and less than " + enteredNumber);
+                    System.out.println("Try Again");
+                    enteredNumber = sc.nextInt();
+                } else if (actualNumber % 2 == 0 && actualNumber > enteredNumber) {
+                    System.out.println("Number is even and greater than " + enteredNumber);
+                    System.out.println("Try Again");
+                    enteredNumber = sc.nextInt();
+                } else if (actualNumber % 2 != 0 && actualNumber < enteredNumber) {
+                    System.out.println("Number is odd and less than " + enteredNumber);
+                    System.out.println("Try Again");
+                    enteredNumber = sc.nextInt();
+                } else if (actualNumber % 2 != 0 && actualNumber > enteredNumber) {
+                    System.out.println("Number is odd and greater than " + enteredNumber);
+                    System.out.println("Try Again");
+                    enteredNumber = sc.nextInt();
+                } else {
+                    System.out.println("Its a wrong guess....! Try Again...");
+                    enteredNumber = sc.nextInt();
+                }
+            }
+            System.out.println("Hurray! You guessed it right....");
+        }
+    }
+    public static int numberGenerator(){
+        int min = 1;
+        int max = 101;
+        double r = Math.random();
+        int number = (int)(r*(max-min))+min;
+        return number;
+    }
+}
